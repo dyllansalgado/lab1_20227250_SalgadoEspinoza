b2302835a1b792623938c8c3e8d3e61a0c083e4a
dyllan salgado <dyllan.salgado@usach.cl>
2020-11-04 11:20:09 -0300

    idea de como iniciar register y login

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Laboratorio1-Scheme
 02-11-2020 //empezando a realizar el laboratorio
+03-11-2020 //ideas de como empezar el login y register
--- TDAPREGUNTA.rkt
+++ TDAPREGUNTA.rkt
@@ -1,12 +1,26 @@
 #lang racket
 
+;Que debo ir creando para la opcion register.
+
+;1)Primero debo crear una lista vacia que solicitara los datos de usuario y contraseña.
+;2)Asignar la lista que sera tipo ( user, pass) a otra lista contenedora llama lista de usuarios.
+;3)Luego debo ver si el nombre de usuario se encuentra en la lista de usuarios (con recursion)
+;4)Si se encuentra debo mostrar por pantalla que el nombre de usuario no se encuentra y que debe volver a registrarse.
+
+;Que debo ir creando para la opcion login.
+
+;1)Si el usuario pudo completar el registro,(nombre correcto y pass), debo pedir que ingrese buscando los datos dentro de la
+;funcion lista de usuarios.
+;2)Debo ir recorriendo la lista hasta encontrar el (nombre y pass), si no se encuentra es porque se equivoco de user o pass.
+
+
 ;Constructores
 ;Creamos una lista vacia que contendra a los usuarios.
-(define (usuariosVacio )
+(define (usuariosvacio )
   (list )
 )
 ;Creamos una lista vacia que tendra las preguntas en el foro.
-(define (foroPreguntasVacio )
+(define (foropreguntasvacio )
   (list )
 )
 
